ee2f345b680b6fd2ef41a495d4c9dd0267261757
Dylan Crawshaw <dcrawsh@gmail.com>
2019-08-20 01:31:36 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,14 +5,14 @@ end
 
 name_array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def batch_badge_creator(array)
-  array.collect do |name|
+  array.map do |name|
   "Hello, my name is #{name}."
 end
 end
 
 def assign_rooms(array)
    counter = 0
-   array.collect do |name|
+   array.each_with_index.map do |name|
    counter += 1
    "Hello, #{name}! You'll be assigned to room #{counter}!"
  end
