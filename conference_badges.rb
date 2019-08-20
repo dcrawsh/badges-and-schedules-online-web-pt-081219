646f9d44e132874ba365c0f36ca3726be242bf79
Dylan Crawshaw <dcrawsh@gmail.com>
2019-08-20 01:30:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def badge_maker(name)
+  puts "Hello, my name is #{name}."
+  "Hello, my name is #{name}."
+end
+
+name_array = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator(array)
+  array.collect do |name|
+  "Hello, my name is #{name}."
+end
+end
+
+def assign_rooms(array)
+   counter = 0
+   array.collect do |name|
+   counter += 1
+   "Hello, #{name}! You'll be assigned to room #{counter}!"
+ end
+ end
+ 
+ def printer(array)
+   puts batch_badge_creator(array)
+   puts assign_rooms(array)
+ end
+  
